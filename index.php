6d48685f5eed2188ea6f6abcc7116e28a8038985
Doni <tambah file html dan css>
2024-09-16 20:21:45 +0900

    tambah file php

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- CSS Bootrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <h1>PHP</h1>
+    <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum quam expedita beatae non nostrum. Necessitatibus quod consequatur odio illo. Et? </p>
+
+
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-link">Link</button>
+
+
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+       CEK ID
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Cek ID</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                   Nama Saya Samuel Bosawer
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- JS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ layout.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- CSS Bootrap  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="row mt-3 ">
+
+            <div class="col-md-12 mt-1 text-white">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+
+            <div class="col-md-6  mt-1 text-white ">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+
+            <div class="col-md-2 bg-info mt-1 text-white ">
+               <h3 class="p-3">3</h3>
+            </div>
+
+            <div class="col-md-2 bg-success mt-1 text-white ">
+               <h3 class="p-3">4</h3>
+            </div>
+
+            <div class="col-md-2 bg-warning mt-1 text-white ">
+               <h3 class="p-3">5</h3>
+            </div>
+
+
+            <div class="col-md-2 bg-secondary mt-1 text-white ">
+               <h3 class="p-3">6</h3>
+            </div>
+
+            <div class="col-md-2 bg-dark mt-1 text-white ">
+               <h3 class="p-3">7</h3>
+            </div>
+
+            <div class="col-md-2 bg-dark mt-1 text-white ">
+               <h3 class="p-3">8</h3>
+            </div>
+
+            <div class="col-md-2 bg-dark mt-1 text-white ">
+               <h3 class="p-3">9</h3>
+            </div>
+
+            <div class="col-md-2 bg-dark mt-1 text-white ">
+               <h3 class="p-3">10</h3>
+            </div>
+
+            <div class="col-md-2 bg-dark mt-1 text-white ">
+               <h3 class="p-3">11</h3>
+            </div>
+
+            <div class="col-md-2 bg-dark mt-1 text-white ">
+               <h3 class="p-3">12</h3>
+            </div>
+
+            
+
+
+
+
+
+        </div>
+
+        <div class="row mt-3 text-center">
+
+<div class="col-md-2 bg-dark mt-1 text-white">
+   <h3 class="p-3">1</h3>
+</div>
+
+<div class="col-md-2 bg-primary mt-1 text-white ">
+   <h3 class="p-3">2</h3>
+</div>
+
+<div class="col-md-2 bg-info mt-1 text-white ">
+   <h3 class="p-3">3</h3>
+</div>
+
+<div class="col-md-2 bg-success mt-1 text-white ">
+   <h3 class="p-3">4</h3>
+</div>
+
+<div class="col-md-2 bg-warning mt-1 text-white ">
+   <h3 class="p-3">5</h3>
+</div>
+
+
+<div class="col-md-2 bg-secondary mt-1 text-white ">
+   <h3 class="p-3">6</h3>
+</div>
+
+<div class="col-md-2 bg-dark mt-1 text-white ">
+   <h3 class="p-3">7</h3>
+</div>
+
+<div class="col-md-2 bg-dark mt-1 text-white ">
+   <h3 class="p-3">8</h3>
+</div>
+
+<div class="col-md-2 bg-dark mt-1 text-white ">
+   <h3 class="p-3">9</h3>
+</div>
+
+<div class="col-md-2 bg-dark mt-1 text-white ">
+   <h3 class="p-3">10</h3>
+</div>
+
+<div class="col-md-2 bg-dark mt-1 text-white ">
+   <h3 class="p-3">11</h3>
+</div>
+
+<div class="col-md-2 bg-dark mt-1 text-white ">
+   <h3 class="p-3">12</h3>
+</div>
+
+
+
+
+
+
+
+</div>
+
+    </div>
+
+
+    
+
+    <!-- JS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
